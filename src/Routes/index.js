009397431e4112657663a541c26e83eb92b6615d
MDeFaria <maria.defariasf@gmail.com>
2023-10-31 22:51:17 -0400

    Ruta getAll

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const getAllDogs = require("../Controllers/getAllDogs");
+const getAllDogsHandler = require("../Handlers/getAllDogsHandler");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +7,6 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.use("/dogs", getAllDogs);
+router.use("/dogs", getAllDogsHandler);
 
 module.exports = router;
