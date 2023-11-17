68ab3c16b4b5f420465c19c2ee8dd1f7f364a559
MDeFaria <maria.defariasf@gmail.com>
2023-11-17 16:14:24 -0400

    t

--- src/Controllers/getTemperaments.js
+++ src/Controllers/getTemperaments.js
@@ -5,20 +5,20 @@ const { API_KEY } = process.env;
 
 const getTemperaments = async () => {
   try {
-    const { data } = await axios.get(
-      `https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`
-    );
-    const temp = await data.map((dog) => dog.temperament);
-    const separate = await temp.join().split(",");
-    const trimed = await separate.map((el) => el.trim());
-
-    await trimed.forEach((temp) => {
-      temp !== "" && Temperament.findOrCreate({ where: { name: temp } });
-    });
-
     const allTemps = await Temperament.findAll();
 
-    if (!allTemps.length) throw new Error("This can't be found yet");
+    if (!allTemps.length) {
+      const { data } = await axios.get(
+        `https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`
+      );
+      const temp = await data.map((dog) => dog.temperament);
+      const separate = await temp.join().split(",");
+      const trimed = await separate.map((el) => el.trim());
+
+      await trimed.forEach((temp) => {
+        temp !== "" && Temperament.findOrCreate({ where: { name: temp } });
+      });
+    }
 
     return allTemps;
   } catch (error) {
