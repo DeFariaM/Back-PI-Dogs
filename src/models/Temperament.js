570be8962a7557187195a92d60fc09a145b0f123
MDeFaria <maria.defariasf@gmail.com>
2023-11-17 22:04:52 -0400

    fixed

--- src/models/Temperament.js
+++ src/models/Temperament.js
@@ -8,6 +8,7 @@ module.exports = (sequelize) => {
         type: DataTypes.INTEGER,
         autoIncrement: true,
         primaryKey: true,
+        allowNull: false,
       },
       name: {
         type: DataTypes.STRING,
